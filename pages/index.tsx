6dfd22e4df58c7c94aea886c4f33f866c06c549f
Paul Armstrong <paul@spaceyak.com>
2019-03-19 19:07:33 -0700

    pull a meetup and display name on home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
+import { fetch } from 'cross-fetch';
 import Link from 'next/link';
 import React from 'react';
 
-const Home = (): React.ReactElement => {
-  return (
-    <div>
-      Hello BendJS 👋{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  );
-};
+interface Props {
+  meetups: Array<Object>;
+}
+
+class Home extends React.Component<Props> {
+  static async getInitialProps() {
+    const meetups = await fetch(
+      'https://api.meetup.com/bendjs/events?photo-host=public&page=20&sig_id=13359878&sig=d052a7d6f8ec6f8e374c2a2665e4aa724208ca5d'
+    )
+      .then(res => res.json())
+      .then(data => Object.values(data));
+    return { meetups };
+  }
+
+  render() {
+    const { meetups } = this.props;
+    return (
+      <div>
+        Hello BendJS 👋
+        <div>Next Meetup: {meetups[0].name}</div>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default Home;
