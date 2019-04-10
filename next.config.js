0c719fe34f493a7f404bef8d39b65aef1236bb17
David Panzarella <david@deven.io>
2019-04-09 20:15:59 -0700

    Adding Ant Design with Less and other goodies... WIP

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-const withSass = require('@zeit/next-sass');
-
-module.exports = withSass({
-  webpack(config, options) {
-    return config;
-  },
-  cssModules: true
-});
+module.exports = () => {
+  /* eslint-disable */
+  const withLess = require('@zeit/next-less')
+  const lessToJS = require('less-vars-to-js')
+  const fs = require('fs')
+  const path = require('path')
+  // Where your antd-custom.less file lives
+  const themeVariables = lessToJS(
+    fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+  )
+  // fix: prevents error when .less files are required by node
+  if (typeof require !== 'undefined') {
+    require.extensions['.less'] = file => {}
+  }
+  return withLess({
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables // make your antd custom effective
+    }
+  })
+};
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Component } from 'react';
 import Link from "next/link";
 import { fetch } from "cross-fetch";
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-import styles from '../styles/styles.scss'
+const { Header, Content, Footer } = Layout;
 
-class Home extends React.Component {
+class Home extends Component {
   static async getInitialProps() {
     const meetups = await fetch(
       "https://api.meetup.com/bendjs/events?photo-host=public&page=20&sig_id=13359878&sig=d052a7d6f8ec6f8e374c2a2665e4aa724208ca5d"
@@ -19,16 +19,37 @@ class Home extends React.Component {
   render() {
     const { meetups } = this.props;
     return (
-      <div>
-        Hello BendJS 👋
-        <div>Next Meetup: {meetups[0].name}</div>
-        <Link href="/about">
-          <a role="link">About</a>
-        </Link>
-        <Link href="/people/">
-          <a role="link">Go to people</a>
-        </Link>
-      </div>
+      <Layout className="layout">
+        <Header>
+          {/* <div className="logo" /> */}
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">
+              <Link href="/about">
+                <a role="link">About</a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link href="/people">
+                <a role="link">Go to people</a>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <h1>Hello BendJS 👋</h1>
+            Next Meetup: {meetups[0].name}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          BendJS ©2019 Created by some badass BendJS devs (yup...its like that)
+        </Footer>
+      </Layout>
     );
   }
 }
